3431ac6e057b9798bbf27de4b780392c157f5e3a
vadvit009 <vadvit009@gmail.com>
2020-10-29 16:42:01 +0200

    first build

---
+++ build/precache-manifest.efa20a64a2ff8156eadd5beb25998ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fca0c3c9bbce88a5e67779cab906a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "968445f05787956af9db",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "dd860fc6f1f4624861f6",
+    "url": "/static/css/main.b3258721.chunk.css"
+  },
+  {
+    "revision": "968445f05787956af9db",
+    "url": "/static/js/2.ba13692d.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.ba13692d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd860fc6f1f4624861f6",
+    "url": "/static/js/main.58e82e7b.chunk.js"
+  },
+  {
+    "revision": "8acef4bd45c4bd308cf1",
+    "url": "/static/js/runtime-main.160ced06.js"
+  }
+]);
