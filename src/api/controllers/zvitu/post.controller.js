31212b3ed05022e7e565e0b5dfbe1cdb9d1a7e0a
vadvit009 <vadvit009@gmail.com>
2020-10-29 21:05:40 +0200

    rozxid zvitu done

--- src/api/controllers/zvitu/post.controller.js
+++ src/api/controllers/zvitu/post.controller.js
@@ -1,11 +1,11 @@
-const { Zvitu } = require('../../models/zvitu');
-const {Dovidnyky:{Workers:{Worker}}} =require('../../models')
+const {Zvitu} = require('../../models/zvitu');
+const {Dovidnyky: {Workers: {Worker}}} = require('../../models')
 const mongoose = require('mongoose');
 
 module.exports = {
     prixodZvitu: async (req, res) => {
         try {
-            const { user, operationId, workerId, gatynok1, gatynok2, gatynok3, date_prixodu } = req.body;
+            const {user, operationId, workerId, gatynok1, gatynok2, gatynok3, date_prixodu} = req.body;
             const prixodZvitu = await Zvitu.create({
                 operationId,
                 workerId,
@@ -24,10 +24,11 @@ module.exports = {
     },
     rozxidZvitu: async (req, res) => {
         try {
-            let { user, operationId, workerId, gatynok1, gatynok2, gatynok3, date_rozxodu } = req.body;
+            let {user, operationId, workerId, gatynok1, gatynok2, gatynok3, date_rozxodu} = req.body;
             const isValidWorker = await Worker.findById(workerId);
-            if (isValidWorker.operationId.includes(operationId)){}
-            const rozxidZvitu = await Zvitu.find({ date_rozxodu: null });
+            if (isValidWorker.operationId.includes(operationId)) {
+            }
+            const rozxidZvitu = await Zvitu.find({date_rozxodu: null});
             const sum = {
                 gatynok1: 0,
                 gatynok2: 0,
@@ -38,7 +39,6 @@ module.exports = {
                 sum.gatynok2 += zvit.gatynok2
                 sum.gatynok3 += zvit.gatynok3
             })
-            // const arrToRequest = [];
 
             if (sum.gatynok1 < gatynok1 /*&& sum.gatynok2 < gatynok2 && sum.gatynok3 < gatynok3*/) {
                 return res.status(400).send('No such Quantity')
@@ -46,13 +46,14 @@ module.exports = {
                 const arrToRequest = rozxidZvitu.map(zvit => {
                     if (gatynok1 > 0) {
                         if (zvit.gatynok1 > gatynok1) {
+                            return {...zvit._doc, gatynok1: zvit.gatynok1 - gatynok1};
                             gatynok1 = 0
-                            return { ...zvit._doc, gatynok1: zvit.gatynok1 - gatynok1 }
+
                             // arrToRequest.push({...zvit._doc, gatynok1: zvit.gatynok1 - gatynok1})
                         } else if (zvit.gatynok1 <= gatynok1) {
                             // arrToRequest.push({...zvit._doc, gatynok1: 0})
                             gatynok1 -= zvit.gatynok1
-                            return { ...zvit._doc, gatynok1: 0 }
+                            return {...zvit._doc, gatynok1: 0}
                         }
                     }
                 })
@@ -61,17 +62,18 @@ module.exports = {
                 arrToRequest.filter(Boolean)
                     .forEach(item => {
                         console.log("item === ", item)
+
                         Zvitu.findByIdAndUpdate(item._id,
                             {
-                                ...item,
                                 changesId: user._id,
                                 date_rozxodu
                             },
-                            { new: true })
+                            {new: true})
                             .then((updatedZvit) => {
                                 console.log("updatedZvit === ", updatedZvit._doc)
                                 Zvitu.create({
                                     ...updatedZvit._doc,
+                                    gatynok1: item.gatynok1,
                                     _id: new mongoose.Types.ObjectId(),
                                     date_rozxodu: null
                                 })
